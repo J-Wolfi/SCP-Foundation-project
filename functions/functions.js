158010800c2433838ae0d3854b2896488296dea4
J-Wolfi <117366589+J-Wolfi@users.noreply.github.com>
2022-11-30 20:16:51 +0100

    a

--- functions/functions.js
+++ functions/functions.js
@@ -1,12 +1,19 @@
 let icon = document.getElementById('icon');
+let content = document.getElementById('content');
 icon.addEventListener('click', function(){
     if(icon.classList.contains('active')){
         icon.classList.remove('active');
-    } else icon.classList.add('active');
+        content.classList.remove('no-click');
+    } 
+    else {
+        icon.classList.add('active');
+        content.classList.add('no-click');
+    } 
 });
 
 window.addEventListener("resize", function() {
     if(icon.classList.contains('active')){
         icon.classList.remove('active');
+        content.classList.remove('no-click');
     }
 });
